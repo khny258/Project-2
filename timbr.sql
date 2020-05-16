ffe3f1fb2026aaf9e18c56ae43627274bdb50fa1
arthur hauser <arthur.hauser@gmail.com>
2020-05-16 10:44:27 -0400

    saturday sql changes

--- timbr.sql
+++ timbr.sql
@@ -11,6 +11,10 @@ email VARCHAR(50) NOT NULL,
 birthday VARCHAR(30) NOT NULL,
 gender_identity VARCHAR(30),
 gender_of_partner VARCHAR(30)
+
+password VARCHAR(12) NOT NULL,
+liked_genres VARCHAR(30) 
+disliked_genres VARCHAR(30)
 );
 
 CREATE TABLE genre_preferences (
@@ -42,3 +46,92 @@ punk BOOLEAN DEFAULT false,
 funk BOOLEAN DEFAULT false
 );
 
+CREATE TABLE user_instruments (
+-- user_id INT UNSIGNED NOT NULL,
+-- INDEX user_ind (user_id),
+-- CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES timbr_user (id) ON DELETE SET NULL,
+Brass BOOLEAN DEFAULT false, 
+Trumpet BOOLEAN DEFAULT false, 
+Cornet BOOLEAN DEFAULT false, 
+Trombone BOOLEAN DEFAULT false, 
+Baritone_Horn BOOLEAN DEFAULT false, 
+French Horn BOOLEAN DEFAULT false, 
+Saxaphone BOOLEAN DEFAULT false, 
+Sousaphone BOOLEAN DEFAULT false, 
+Tuba BOOLEAN DEFAULT false, 
+Wagner Tuba BOOLEAN DEFAULT false, 
+Percussion BOOLEAN DEFAULT false, 
+Triangle BOOLEAN DEFAULT false, 
+Timpani BOOLEAN DEFAULT false, 
+Xylophone BOOLEAN DEFAULT false, 
+Vibraphone BOOLEAN DEFAULT false, 
+Marimba BOOLEAN DEFAULT false, 
+Tubular_Bells BOOLEAN DEFAULT false, 
+Glockenspiel BOOLEAN DEFAULT false, 
+Celesta BOOLEAN DEFAULT false, 
+Bass_Drum BOOLEAN DEFAULT false, 
+Snare_Drum BOOLEAN DEFAULT false, 
+Wood_Block BOOLEAN DEFAULT false, 
+Whip_Block BOOLEAN DEFAULT false, 
+Ratchet BOOLEAN DEFAULT false, 
+Cymbals BOOLEAN DEFAULT false, 
+Strings BOOLEAN DEFAULT false, 
+Violin BOOLEAN DEFAULT false, 
+Viola BOOLEAN DEFAULT false, 
+Cello BOOLEAN DEFAULT false, 
+Bass BOOLEAN DEFAULT false, 
+Harp BOOLEAN DEFAULT false, 
+Guitar BOOLEAN DEFAULT false, 
+Viol_de_gamba BOOLEAN DEFAULT false, 
+Theorbo BOOLEAN DEFAULT false, 
+Woodwinds BOOLEAN DEFAULT false, 
+Piccolo BOOLEAN DEFAULT false, 
+Flute BOOLEAN DEFAULT false, 
+Clarinet BOOLEAN DEFAULT false, 
+Oboe BOOLEAN DEFAULT false, 
+Bass_Clarinet BOOLEAN DEFAULT false, 
+Bassoon BOOLEAN DEFAULT false, 
+Contrabassoon BOOLEAN DEFAULT false, 
+Bagpipe BOOLEAN DEFAULT false, 
+Harmonica BOOLEAN DEFAULT false, 
+Keyboard BOOLEAN DEFAULT false, 
+Piano BOOLEAN DEFAULT false, 
+Harpsichord BOOLEAN DEFAULT false, 
+Clavichord BOOLEAN DEFAULT false, 
+Accordion BOOLEAN DEFAULT false, 
+Celesta BOOLEAN DEFAULT false, 
+Harmonium BOOLEAN DEFAULT false, 
+Voice BOOLEAN DEFAULT false, 
+Soprano BOOLEAN DEFAULT false, 
+Mezzo_Soprano BOOLEAN DEFAULT false, 
+Contralto BOOLEAN DEFAULT false, 
+Countertenor BOOLEAN DEFAULT false, 
+Tenor BOOLEAN DEFAULT false, 
+Baritone BOOLEAN DEFAULT false, 
+Bass_voice BOOLEAN DEFAULT false, 
+
+password VARCHAR(12) NOT NULL,
+liked_genres VARCHAR(30) 
+disliked_genres VARCHAR(30)
+);
+
+INSERT INTO timbr_user (f_name, l_name, email, birthday, gender_identity, gender_of_partner)
+values ("John", "Marimba", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Peter", "Ratchet", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Garry", "Bass Drum", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Leslie", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Jacob", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Daniel", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Alex", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Ryan", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Christopher", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Aaron", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Michael", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Adam", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Harry", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Anthony", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Sebastian", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Darren", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Luke", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Sam", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
+values ("Xavier", "trombone", "john@trombone.com", "01/10/1970", "trombone", "tuba")
